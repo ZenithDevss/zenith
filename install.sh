705450919c8a810bd10612b02832c38dbdb06039
emma <264767106+emmadumi@users.noreply.github.com>
2026-03-06 16:49:27 +0100

    sistemato install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ echo "Installazione Zenith..."
 git clone https://github.com/ZenithDevss/zenith ~/zenith
 
 # Installazione componenti Zenith
-sudo snap install neovim --classic
+sudo snap install nvim --classic
 
 # Crea link simbolico per configurazione neovim
-ls -s ~zenith/nvim ~/.config/nvim
+ls -s ~/zenith/nvim ~/.config/nvim
 
 echo "Fatto!"
