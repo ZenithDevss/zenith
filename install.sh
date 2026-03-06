acfc42410f5f7288aff198f152fe02ecd781bbe6
emma <264767106+emmadumi@users.noreply.github.com>
2026-03-06 17:13:23 +0100

    miglioramento install.sh e risoluzione bug

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 echo "Installazione Zenith..."
 
-# Clona la repo
-git clone https://github.com/ZenithDevss/zenith ~/zenith
+# Clona la repo se non c'è già
+if [ ! -d ~/zenith ]; then
+  git clone https://github.com/ZenithDevss/zenith ~/zenith
+fi
 
 # Installazione componenti Zenith
 sudo snap install nvim --classic
 
 # Crea link simbolico per configurazione neovim
-ls -s ~/zenith/nvim ~/.config/nvim
+ln -s ~/zenith/nvim ~/.config/nvim
 
 echo "Fatto!"
