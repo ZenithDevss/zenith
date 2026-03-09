206178ac7a55b0aeb283c77f971265a34c67c6be
giulia <264767106+emmadumi@users.noreply.github.com>
2026-03-09 21:58:50 +0100

    Installate dipendenze necessarie per neovim

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ if [ ! -d ~/zenith ]; then
   git clone https://github.com/ZenithDevss/zenith ~/zenith
 fi
 
+# Installazione dipendenze neovim
+sudo apt install -y gcc curl git
+
 # Installazione componenti Zenith
 sudo snap install nvim --classic
 
 # Crea link simbolico per configurazione neovim
 ln -s ~/zenith/nvim ~/.config/nvim
 
-echo "bash ~/zenith/welcome.sh" >> ~/.bashrc
+echo "bash ~/zenith/welcome.sh" >>~/.bashrc
 
 echo "Fatto!"
