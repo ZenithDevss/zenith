8e15a968e2a9827ece1ecf50c0bacf8ab28c6677
giulia <264767106+emmadumi@users.noreply.github.com>
2026-03-09 23:00:00 +0100

    aggiunto fastfetch configurato

--- install.sh
+++ install.sh
@@ -7,14 +7,20 @@ if [ ! -d ~/zenith ]; then
   git clone https://github.com/ZenithDevss/zenith ~/zenith
 fi
 
-# Installazione dipendenze neovim
-sudo apt install -y gcc curl git
-
-# Installazione componenti Zenith
+#---------------------------------
+# --> Installazione Componenti <--
+#---------------------------------
+# Neovim + dipendenze
+sudo apt install -y gcc curl
 sudo snap install nvim --classic
+# Fastfetch
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+sudo apt update
+sudo apt install -y fastfetch
 
-# Crea link simbolico per configurazione neovim
+# Crea link simbolico per configurazioni varie
 ln -s ~/zenith/nvim ~/.config/nvim
+ln -s ~/zenith/fastfetch ~/.config/fastfetch
 
 echo "bash ~/zenith/welcome.sh" >>~/.bashrc
 
